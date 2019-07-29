632c54f862868e5ca6fd831d303aa2729cfed241
trung <>
2019-07-29 21:29:42 +0700

    Add test with ReactJS boilerplatte

---
+++ test-reactjs/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FeeSimple from 'feesimple-js';
+const feesimpleClient = FeeSimple();
+
+function App() {
+  
+  const accountName = 'usertrung123';
+  feesimpleClient.getAvailabilityData(accountName)
+    .then(availabilityData => {
+      console.log('availabilityData:', JSON.stringify(availabilityData, null, 2));
+    })
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
